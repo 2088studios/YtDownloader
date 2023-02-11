999b3c563c650c328dc317c6a5299901a38f77db
2088studios <2088studios@gmail.com>
2023-02-11 11:26:42 -0600

    Updated readme and changed script to automatically get 'Downloads' folder path, as well as ask user for URL input for when script becomes .exe

--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
-YouTube Video Downloader
+**YouTube Video Downloader**
 
 This program downloads videos at the requested quality chosen by the given URL.
 
-Initial Coding Tutorial Here -> https://youtu.be/vEQ8CXFWLZU?t=320
-Library Here -> https://pytube.io/en/latest/
+[Pytube Library Here](https://pytube.io/en/latest/)
 
-HOW TO USE/INSTALL
+**HOW TO USE/INSTALL**
 1. ?
 
-Enjoy! :)
+**Enjoy! :)**
+
+*Last updated ~ 2/9/2023*
--- ytdownloader.py
+++ ytdownloader.py
@@ -1,25 +1,30 @@
-# Coding Tutorial Here -> https://youtu.be/vEQ8CXFWLZU?t=320
 # Library Here -> https://pytube.io/en/latest/
 
 #TODO Figure out how to choose download quality (from ???-4k)
 #TODO Figure out how to get this into an executable file
+#TODO Add exception blocks
 
-#imports
+#Libraries to get YouTube video and users' Downloads folder path
 from pytube import YouTube
-from sys import argv
+from pathlib import Path
 
-#
-link = argv[1]
-yt = YouTube(link)
+#Gets video URL from user
+url = input('Hello, please input video URL\n')
 
-#
-print("Video Title: ", yt.title)
+#Creates YouTube object and assigns users' Downloads folder path
+video = YouTube(url)
+download_path = str(Path.home() / "Downloads")
 
-#
-print("Views: ", yt.views)
+#Prints video data
+print("Video Title: ", video.title)
+print("Views: ", video.views)
 
-#
-yd = yt.streams.get_highest_resolution()
+#Gets highest quality video (up to 720 as of now) and then downloads it
+dv = video.streams.get_highest_resolution()
+dv.download(download_path)
 
-#Downloads video to specified path
-yd.download(output_path:=r'C:\Users\isali\Downloads')
+#TODO check if video has already been downloaded
+#if yt.title==:
+#    print("There is already a video with that name. Cannot download video")
+
+print("Video has been downloaded successfully!")
