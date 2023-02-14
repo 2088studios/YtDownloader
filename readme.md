0e5803883b8b27bf84c50f84c1e0e4fcae12b928
2088studios <2088studios@gmail.com>
2023-02-13 19:35:48 -0600

    Fixed bug where script wasn't reading input to stop running

--- readme.md
+++ readme.md
@@ -5,8 +5,11 @@ This program downloads videos chosen by the given URL at the requested quality
 [Pytube Library Here](https://pytube.io/en/latest/)
 
 **HOW TO USE/INSTALL**
-1. ?
+1. Run ytdownloader file in "dist" folder
+2. Follow prompts
+3. 
+4. Profit!
 
 **Enjoy! :)**
 
-*Last updated ~ 2/11/2023*
+*Last updated ~ 2/13/2023*
--- ytdownloader.py
+++ ytdownloader.py
@@ -1,10 +1,14 @@
 # Library Here -> https://pytube.io/en/latest/
 
-#TODO Figure out how to choose download quality (from ???-4k)
-#TODO Figure out how to get this into an executable file
-#TODO Add exception blocks
+#TODO How to choose download quality (from ???-4k)
+#TODO Run on windows[X] and macos[]
+#TODO Add exception blocks for downloaded video
+#Video already downloaded[]
+#
+#TODO add script if a URL insn't provided
 #TODO Add way to download multiple videos back to back
-#TODO check if video has already been downloaded
+
+#Check if video has already been downloaded
 #if yt.title==:
 #    print("There is already a video with that name. Cannot download video")
 
@@ -18,7 +22,7 @@ download_path = str(Path.home() / "Downloads")
 #Function that downloads the YouTube video
 def main():
     #Gets video URL from user
-    url = input('Hello, please input video URL\n')
+    url = input('Please input video URL\n')
 
     #Creates YouTube object and assigns users' Downloads folder path
     video = YouTube(url)
@@ -40,11 +44,11 @@ def anotherVideo():
     k = input('Would you like to download another video? (Y/N)\n')
 
     #User wants to download another
-    if (k=='Y' or 'y'):
+    if (k.lower()=='y'):
         main()
 
     #User doesn't want to download another
-    elif(k=='N' or 'n'):
+    elif(k.lower()=='n'):
         print('Thank you for using the downloader.')
         print('It will automatically close in 10 seconds.')
         time.sleep(10)
