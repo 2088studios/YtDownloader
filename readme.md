6ec24723428ca94cda756a8f5ee75fc4851f75a7
2088studios <2088studios@gmail.com>
2023-02-22 14:05:20 -0600

    Addded multiple methods and added audio download functionality

--- readme.md
+++ readme.md
@@ -5,11 +5,12 @@ This program downloads videos chosen by the given URL at the requested quality
 [Pytube Library Here](https://pytube.io/en/latest/)
 
 **HOW TO USE/INSTALL**
+
 1. Run ytdownloader file in "dist" folder
 2. Follow prompts
-3. 
+3.
 4. Profit!
 
 **Enjoy! :)**
 
-*Last updated ~ 2/13/2023*
+_Last updated ~ 2/17/2023_
--- ytdownloader.py
+++ ytdownloader.py
@@ -1,60 +1,98 @@
 # Library Here -> https://pytube.io/en/latest/
 
-#TODO How to choose download quality (from ???-4k)
-#TODO Run on windows[X] and macos[]
-#TODO Add exception blocks for downloaded video
-#Video already downloaded[]
+# TODO How to choose download quality (from ???-4a)
+# TODO Run on windows[X] and macos[]
+# TODO Add exception blocas for downloaded video
+# Video already downloaded[]
 #
-#TODO add script if a URL insn't provided
-#TODO Add way to download multiple videos back to back
+# TODO See if You can download audio and not video
 
-#Check if video has already been downloaded
-#if yt.title==:
+# Checa if video has already been downloaded
+# if yt.title==:
 #    print("There is already a video with that name. Cannot download video")
 
-#Libraries to get YouTube video and users' Downloads folder path
+# Libraries to get YouTube video and users' Downloads folder path
 from pytube import YouTube
 from pathlib import Path
 import time
 
 download_path = str(Path.home() / "Downloads")
 
-#Function that downloads the YouTube video
-def main():
-    #Gets video URL from user
-    url = input('Please input video URL\n')
 
-    #Creates YouTube object and assigns users' Downloads folder path
+def main():
+    print("Hello and welcome to YtDownloader! :)")
+    getURL()
+
+
+def getURL():
+    # Gets video URL from user
+    url = 'a'
+
+    # TODO add script if a URL insn't provided (close but can still breaa if input is just http:/)
+    while not url.lower().startswith("https://www.youtube.com/"):
+        url = input(
+            'Please input a valid video URL "https://www.youtube.com/..."\n')
+
+    # Creates YouTube object and assigns users' Downloads folder path
     video = YouTube(url)
 
-    #Prints video data
+    # Prints video data
     print("Video Title: ", video.title)
     print("Views: ", video.views)
-    print('Downloading...')
+    chooseDownload(video)
 
-    #Gets highest quality video (up to 720 as of now) and then downloads it
+
+def chooseDownload(video):
+    c = input('Press [V] for video and audio. Or [A] for audio only\n')
+    # Video and audio
+    if (c.lower() == 'v'):
+        videoAndAudio(video)
+
+    # Audio only
+    elif (c.lower() == 'a'):
+        audioOnly(video)
+
+    # Incorrect input
+    else:
+        chooseDownload()
+
+
+def videoAndAudio(video):
+    # Gets highest quality video (up to 720 as of now) and then downloads it
     dv = video.streams.get_highest_resolution()
     dv.download(download_path)
 
     print("Video has been downloaded successfully!")
     anotherVideo()
 
-#Asks user if they want to download another video
+
+def audioOnly(video):
+    da = video.streams.get_audio_only()
+    da.download(download_path)
+    print("Audio has been downloaded successfully!")
+    anotherVideo()
+
+
 def anotherVideo():
-    k = input('Would you like to download another video? (Y/N)\n')
+    a = input('Would you like to download another video? (Y/N)\n')
 
-    #User wants to download another
-    if (k.lower()=='y'):
-        main()
+    # User wants to download another
+    if (a.lower() == 'y'):
+        getURL()
 
-    #User doesn't want to download another
-    elif(k.lower()=='n'):
-        print('Thank you for using the downloader.')
-        print('It will automatically close in 10 seconds.')
-        time.sleep(10)
+    # User doesn't want to download another
+    elif (a.lower() == 'n'):
+        closeProgram()
 
-    #Incorrect input
+    # Incorrect input
     else:
         anotherVideo()
+
+
+def closeProgram():
+    print('Thank you for using YtDownloader!')
+    print('It will automatically close in 10 seconds.')
+    time.sleep(10)
+
 
 main()
